6b142966bd6d59c76480bec4cad43bf6b2a9f640
Mayara Meireles <mayarameirelessantos@gmail.com>
2021-04-29 22:43:23 -0300

    Projeto finalizado e Pronto para uso

---
+++ Jogo_Xadrez/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Xadrez;
+using tabuleiro;
+
+namespace Jogo_Xadrez
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{   
+				PartidaDeXadrez partida = new PartidaDeXadrez();
+				while (!partida.terminada)
+				{
+					try
+					{
+					Console.Clear();
+					Tela.imprimirPartida(partida);
+
+					Console.WriteLine();
+					Console.Write("Origem: ");
+					Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+					partida.validarPosicaoDeOrigem(origem);
+					bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+					Console.Clear();
+
+					Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+					Console.WriteLine();
+					Console.Write("Destino: ");
+					Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+					partida.validarPosicaoDeDestino(origem, destino);
+					partida.realizarJogada(origem, destino);
+					}
+					catch(TabuleiroException ex)
+					{
+						Console.WriteLine(ex.Message);
+						Console.ReadLine();
+					}
+				}
+				Console.Clear();
+				Tela.imprimirPartida(partida);
+			}
+			catch (TabuleiroException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+	}
+}
+
---
+++ Jogo_Xadrez/tabuleiro/Cor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+	enum Cor 
+	{
+		Vermelho,
+		Azul,
+		Verde,
+		Preto,
+		Branco,
+		Amarelo
+	}
+}
